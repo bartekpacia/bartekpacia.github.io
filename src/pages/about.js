36eab2e34f41b6d9752a0fad7987f65ee43b54bf
Bartek Pacia <barpac02@gmail.com>
2020-10-03 15:25:24 +0200

    added empty pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <p>About page</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
